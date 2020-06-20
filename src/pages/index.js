c010893fbb36d299118f8f3e2d6267cdd32395ea
abtin98 <abtin.ameri@mail.mcgill.ca>
2020-06-20 18:46:51 -0400

    initial additions to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,29 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+  <div>
+    <h1>
+      Abtin Ameri
+    </h1>
+    <h2>
+      <p>
+        <a href="https://www.mit.edu">MIT</a> PhD Student
+      </p>
+      <p>
+        2016 Schulich Leader Scholar
+      </p>
+      <p>
+        Ernerst brown Gold Medalist for highest ability throughout the undergraduate program.
+      </p>
+      <p>
+        Co-founder and instructor of the McGill Physics Olympiad Program (MPOP)
+      </p>
+      <p>
+        Former Vice President Academic of the Engineering Undergraduate Society
+      </p>
+      
+
+    </h2>
+  </div> )
 }
